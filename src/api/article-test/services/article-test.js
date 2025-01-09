3890de6006bf609af6b453d28bde2b8a1ce0c694
ptisserant-moustachebikes <p.tisserant@moustachebikes.com>
2025-01-09 11:24:16 +0100

    real add b2c blog

--- src/api/article-test/services/article-test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * article-test service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::article-test.article-test');
